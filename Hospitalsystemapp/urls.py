d03d8d63d58ccebab08650071d29c548467ea20f
cherop-26 <cherrymutai@gmail.com>
2025-02-25 16:32:28 +0300

    seventh save

--- Hospitalsystemapp/urls.py
+++ Hospitalsystemapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('starter/',views.starter,name='starter'),
+    path('about/',views.about,name='about'),
+    path('service/',views.service,name='service'),
+    path('departments/',views.departments,name='departments'),
+    path('doctors/',views.doctors,name='doctors'),
 ]
